c3be1d60db506395224277be1ce5ac3b0fbe701a
zacky1972 <zacky1972@gmail.com>
2019-01-11 18:09:31 +0900

    initial commit

---
+++ lib/sum_mag.ex
@@ -0,0 +1,18 @@
+defmodule SumMag do
+  @moduledoc """
+  Documentation for SumMag.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SumMag.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sum_mag_test.exs
@@ -0,0 +1,8 @@
+defmodule SumMagTest do
+  use ExUnit.Case
+  doctest SumMag
+
+  test "greets the world" do
+    assert SumMag.hello() == :world
+  end
+end
