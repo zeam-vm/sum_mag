2ed7eb327126d5b7fd527888e177823950aab5be
zacky1972 <zacky1972@gmail.com>
2019-01-11 21:38:30 +0900

    create parse_args

--- lib/sum_mag.ex
+++ lib/sum_mag.ex
@@ -4,16 +4,37 @@ defmodule SumMag do
   """
 
   @doc """
-  	## Examples
+    ## Examples
 
-  	iex> [] |> SumMag.convert_args
-  	[]
+    iex> [{:func, [context: Elixir], []}, [do: {:nil, [], Elixir}]] |> SumMag.parse_args
+    []
 
-  	iex> [{:a, [], Elixir}] |> SumMag.convert_args
-  	[:a]
+    iex> [{:func, [context: Elixir], [{:a, [], Elixir}]}, [do: {:a, [], Elixir}]] |> SumMag.parse_args
+    [:a]
 
-  	iex> [{:a, [], Elixir}, {:b, [], Elixir}] |> SumMag.convert_args
-  	[:a, :b]
+    iex> [{:func, [context: Elixir], [{:a, [], Elixir}, {:b, [], Elixir}]},[do: {:+, [context: Elixir, import: Kernel], [{:a, [], Elixir}, {:b, [], Elixir}]}]] |> SumMag.parse_args
+    [:a, :b]
+
+
+  """
+  def parse_args(body) do
+    body
+    |> hd
+    |> elem(2)
+    |> convert_args()
+  end
+
+  @doc """
+    ## Examples
+
+    iex> [] |> SumMag.convert_args
+    []
+
+    iex> [{:a, [], Elixir}] |> SumMag.convert_args
+    [:a]
+
+    iex> [{:a, [], Elixir}, {:b, [], Elixir}] |> SumMag.convert_args
+    [:a, :b]
   """
   def convert_args(arg_list), do: arg_list |> Enum.map(& elem(&1, 0))
 
