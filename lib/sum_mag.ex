ea9d35e2b33d117bcbdea1820330e7d966208536
zacky1972 <zacky1972@gmail.com>
2019-01-11 21:14:26 +0900

    remove test

--- lib/sum_mag.ex
+++ lib/sum_mag.ex
@@ -2,17 +2,4 @@ defmodule SumMag do
   @moduledoc """
   Documentation for SumMag.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SumMag.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/sum_mag_test.exs
+++ test/sum_mag_test.exs
@@ -1,8 +1,4 @@
 defmodule SumMagTest do
   use ExUnit.Case
   doctest SumMag
-
-  test "greets the world" do
-    assert SumMag.hello() == :world
-  end
 end
