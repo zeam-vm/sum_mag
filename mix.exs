30c28bd14d331dd1635a53c60c089753fc13199e
zacky1972 <zacky1972@gmail.com>
2019-01-11 21:15:15 +0900

    version 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SumMag.MixProject do
   def project do
     [
       app: :sum_mag,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
