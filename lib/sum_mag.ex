e10d357d91c535127ad9a065a13c94f41656e51d
zacky1972 <zacky1972@gmail.com>
2019-01-11 21:23:22 +0900

    create convert_args

--- lib/sum_mag.ex
+++ lib/sum_mag.ex
@@ -2,4 +2,19 @@ defmodule SumMag do
   @moduledoc """
   SumMag: a meta-programming library for Hastega and Cockatorice.
   """
+
+  @doc """
+  	## Examples
+
+  	iex> [] |> SumMag.convert_args
+  	[]
+
+  	iex> [{:a, [], Elixir}] |> SumMag.convert_args
+  	[:a]
+
+  	iex> [{:a, [], Elixir}, {:b, [], Elixir}] |> SumMag.convert_args
+  	[:a, :b]
+  """
+  def convert_args(arg_list), do: arg_list |> Enum.map(& elem(&1, 0))
+
 end
